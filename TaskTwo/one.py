2844db92efedab504fcd402fc79b08067a5743ba
Meghna-96 <85355446+Meghna-96@users.noreply.github.com>
2021-06-30 02:54:27 -0400

    1

---
+++ TaskTwo/one.py
@@ -0,0 +1,11 @@
+def divisible(int):
+  if (int%3 == 0) and (int%5 == 0):
+    print('Consultadd - Python Training')
+  elif (int%3 == 0):
+    print('Consultadd')
+  elif (int%5 == 0):
+    print('Python Training')
+
+divisible(15)
+divisible(9)
+divisible(10)
