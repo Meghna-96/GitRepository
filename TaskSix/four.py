a51aafae6c1c36a9888fa097303b3bc7e3c51a7b
Meghna-96 <85355446+Meghna-96@users.noreply.github.com>
2021-06-30 14:11:26 -0400

    4

--- TaskSix/four.py
+++ TaskSix/four.py
@@ -1,1 +1,8 @@
+String = 'Consultadd Training'
+def gen_reverse(string):
+  for i in range(len(string)-1,-1,-1):
+    yield string[i]
 
+rev_string = gen_reverse(String)
+for i in rev_string:
+  print(i, end="")
